futurewei-cloud/chogori-platform

commit 8a3931ba3be01a995db40b78319515d55a112b35

Fix task list crash (initialize task list type)

src/common/PartitionMetadata.h
@@ -29,11 +29,21 @@ struct PartitionAssignmentId
     PartitionId id;
     PartitionVersion version;
 
-    K2_PAYLOAD_COPYABLE
+    K2_PAYLOAD_COPYABLE;
 
+    PartitionAssignmentId() : id(0), version {0} { }
     PartitionAssignmentId(PartitionId id, PartitionVersion version) : id(id), version(version) { }
     PartitionAssignmentId(const PartitionAssignmentId&) = default;
     PartitionAssignmentId& operator=(PartitionAssignmentId& other) = default;
+
+    bool parse(const char* str)
+    {
+        if(!str || !*str)
+            return false;
+
+        static_assert(sizeof(id) == sizeof(uint64_t) && sizeof(version.range) == sizeof(uint16_t) && sizeof(version.assign) == sizeof(uint16_t));
+        return sscanf(str, "%lu,%hu,%hu", &id, &version.range, &version.assign) == 3;
+    }
 };
 
 //

src/node/TaskRequest.h
@@ -11,7 +11,7 @@ class AssignmentManager;
 class Partition;
 
 //
-//
+//  List in which current task resides
 //
 enum class TaskListType
 {
@@ -60,14 +60,12 @@ class TaskRequest
 protected:
     Partition& partition;
     MemoryArena arena;  //  Task local memory
-
     TimeTracker timeTracker;
 
-    K2_LINKED_LIST_NODE
-
+    K2_LINKED_LIST_NODE;
     TaskListType ownerTaskList; //  Task list in which this task resides
 
-    TaskRequest(Partition& partition) : partition(partition) {}
+    TaskRequest(Partition& partition) : partition(partition), ownerTaskList(TaskListType::None) {}
 
     enum class ProcessResult
     {

src/common/IntrusiveLinkedList.h
@@ -52,14 +52,14 @@ protected:
     T* tail = nullptr;
     size_t count = 0;
 
-    static IntrusiveLinkedListNode& getNode(T& element) { return element.K2_LINKED_LIST_NODE_VARIABLE; }
-    static T*& prev(T& element) { return (T*&)element.K2_LINKED_LIST_NODE_VARIABLE.prev; }
-    static T*& next(T& element) { return (T*&)element.K2_LINKED_LIST_NODE_VARIABLE.next; }
+    static IntrusiveLinkedListNode& node(T& element) { return element.K2_LINKED_LIST_NODE_VARIABLE; }
+    static T*& prev(T& element) { return (T*&)node(element).prev; }
+    static T*& next(T& element) { return (T*&)node(element).next; }
 
     static T*& prev(T* element) { return prev(*element); }
     static T*& next(T* element) { return next(*element); }
 
-    static bool notLinked(T& element) { return getNode(element).isEmpty(); }
+    static bool notLinked(T& element) { return node(element).isEmpty(); }
 
 public:
 
@@ -77,6 +77,7 @@ public:
         else
             tail = prev(element);
 
+        node(element).clear();
         count--;
     }
 

commit aefdabf977d8fc559b71eb5c2936df13a0e2595a

Fix the build break caused by commit 8a3931ba3be01a995db40b78319515d55a112b35

src/common/PartitionMetadata.h
@@ -29,9 +29,8 @@ struct PartitionAssignmentId
     PartitionId id;
     PartitionVersion version;
 
-    K2_PAYLOAD_COPYABLE;
+    K2_PAYLOAD_COPYABLE
 
-    PartitionAssignmentId() : id(0), version {0} { }
     PartitionAssignmentId(PartitionId id, PartitionVersion version) : id(id), version(version) { }
     PartitionAssignmentId(const PartitionAssignmentId&) = default;
     PartitionAssignmentId& operator=(PartitionAssignmentId& other) = default;

src/node/TaskRequest.h
@@ -62,7 +62,7 @@ protected:
     MemoryArena arena;  //  Task local memory
     TimeTracker timeTracker;
 
-    K2_LINKED_LIST_NODE;
+    K2_LINKED_LIST_NODE
     TaskListType ownerTaskList; //  Task list in which this task resides
 
     TaskRequest(Partition& partition) : partition(partition), ownerTaskList(TaskListType::None) {}