d192fff95270ecefe8497f815cc75b0462c0cd8a
Ivan Avramov <iavramov@futurewei.com>
2020-02-13 12:04:19 -0800

    Templetize value for K2 indexer

--- src/k2/indexer/HOTIndexer.h
+++ src/k2/indexer/HOTIndexer.h
@@ -11,9 +11,10 @@ using namespace std;
 namespace k2
 {
 
+template <typename ValueType>
 struct KeyValuePair {
     String key;
-    std::unique_ptr<VersionedTreeNode> value;
+    std::unique_ptr<VersionedTreeNode<ValueType>> value;
 };
 
 template<typename ValueType>
@@ -26,15 +27,85 @@ struct KeyValuePairKeyExtractor {
     }
 };
 
-class HOTIndexer : public IndexerInterface<HOTIndexer> {
-    using KeyValuePairTrieType = hot::singlethreaded::HOTSingleThreaded<KeyValuePair*, KeyValuePairKeyExtractor>;
+template <typename ValueType>
+class HOTIndexer {
+    using KeyValuePairTrieType = hot::singlethreaded::HOTSingleThreaded<KeyValuePair<ValueType>*, KeyValuePairKeyExtractor>;
     KeyValuePairTrieType m_keyValuePairTrie;
 public:
-    void insert(String key, String value, uint64_t version);
+ void insert(String key, ValueType value, uint64_t version);
 
-    VersionedTreeNode* find(const String& key, uint64_t version);
+ VersionedTreeNode<ValueType>* find(const String& key, uint64_t version);
 
-    void trim(const String& key, uint64_t version);
+ void trim(const String& key, uint64_t version);
 };
+
+template <typename ValueType>
+inline void HOTIndexer<ValueType>::insert(String key, ValueType value, uint64_t version) {
+    std::unique_ptr < VersionedTreeNode<ValueType>> newNode(new VersionedTreeNode<ValueType>());
+    newNode->value = std::move(value);
+    newNode->version = version;
+
+    KeyValuePair<ValueType>* keyValuePair = new KeyValuePair<ValueType>();
+    keyValuePair->key = std::move(key);
+    keyValuePair->value = std::move(newNode);
+
+    auto inserted = m_keyValuePairTrie.insert(keyValuePair, keyValuePair->key.length());
+    //  Value already exists
+    if (!inserted) {
+        auto exist = m_keyValuePairTrie.lookup(keyValuePair->key.c_str(), keyValuePair->key.length());
+        newNode->next = std::move(exist.mValue->value);
+        exist.mValue->value = std::move(newNode);
+    }
+}
+
+template <typename ValueType>
+inline VersionedTreeNode<ValueType>* HOTIndexer<ValueType>::find(const String& key, uint64_t version) {
+    //
+    // Bug: sometimes find() cannot find existing key
+    //
+    // auto it = m_keyValuePairTrie.find(key.c_str());
+    // if (it == m_keyValuePairTrie.end()) {
+    //     return nullptr;
+    // }
+
+    // VersionedTreeNode* node = (*it)->value.get();
+
+    auto it = m_keyValuePairTrie.lookup(key.c_str(), key.length());
+    if (!it.mIsValid) {
+        return nullptr;
+    }
+
+    VersionedTreeNode<ValueType>* node = it.mValue->value.get();
+    while (node && node->version > version) {
+        node = node->next.get();
+    }
+    return node;
+}
+
+template <typename ValueType>
+inline void HOTIndexer<ValueType>::trim(const String& key, uint64_t version) {
+    if (version == std::numeric_limits<uint64_t>::max()) {
+        auto it = m_keyValuePairTrie.lookup(key.c_str(), key.length());
+        if (!it.mIsValid) {
+            return;
+        }
+        m_keyValuePairTrie.remove(key.c_str(), key.length());
+        return;
+    }
+
+    auto it = m_keyValuePairTrie.lookup(key.c_str(), key.length());
+    if (!it.mIsValid) {
+        return;
+    }
+
+    VersionedTreeNode<ValueType>* node = it.mValue->value.get();
+    if (node) {
+        node = node->next.get();
+    }
+    while (node && node->version >= version) {
+        node = node->next.get();
+    }
+    node->next = nullptr;
+}
 
 }
--- src/k2/indexer/MapIndexer.h
+++ src/k2/indexer/MapIndexer.h
@@ -5,14 +5,66 @@
 
 namespace k2
 {
-class MapIndexer : public IndexerInterface<MapIndexer> {
-private:
-    std::map<String, std::unique_ptr<VersionedTreeNode>> m_map;
-public:
-    void insert(String key, String value, uint64_t version);
+template <typename ValueType>
+class MapIndexer {
+   private:
+    std::map < String, std::unique_ptr<VersionedTreeNode<ValueType>> > m_map;
 
-    VersionedTreeNode* find(const String& key, uint64_t version);
+   public:
+    void insert(String key, ValueType value, uint64_t version);
+
+    VersionedTreeNode<ValueType>* find(const String& key, uint64_t version);
 
     void trim(const String& key, uint64_t version);
 };
+
+template <typename ValueType>
+inline void MapIndexer<ValueType>::insert(String key, ValueType value, uint64_t version) {
+    std::unique_ptr<VersionedTreeNode<ValueType>> newNode(new VersionedTreeNode < ValueType>());
+    newNode->value = std::move(value);
+    newNode->version = version;
+
+    auto emplaceResult = m_map.try_emplace(std::move(key), std::move(newNode));
+    //  Value already exists
+    if (!emplaceResult.second) {
+        newNode->next = std::move(emplaceResult.first->second);
+        emplaceResult.first->second = std::move(newNode);
+    }
+}
+
+template <typename ValueType>
+inline VersionedTreeNode<ValueType>* MapIndexer<ValueType>::find(const String& key, uint64_t version) {
+    auto it = m_map.find(key);
+    if (it == m_map.end()) {
+        return nullptr;
+    }
+
+    VersionedTreeNode<ValueType>* node = it->second.get();
+    while (node && node->version > version) {
+        node = node->next.get();
+    }
+    return node;
+}
+
+template <typename ValueType>
+inline void MapIndexer<ValueType>::trim(const String& key, uint64_t version) {
+    if (version == std::numeric_limits<uint64_t>::max()) {
+        m_map.erase(key);
+        return;
+    }
+
+    auto it = m_map.find(key);
+    if (it == m_map.end()) {
+        return;
+    }
+
+    VersionedTreeNode<ValueType>* node = it->second.get();
+    if (node) {
+        node = node->next.get();
+    }
+    while (node && node->version >= version) {
+        node = node->next.get();
+    }
+    node->next = nullptr;
+}
 }
