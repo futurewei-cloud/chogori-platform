74c01b304fe3d35455808a4ebae3db0ceb3d7a62
Ivan Avramov <iavramov@futurewei.com>
2020-04-20 14:02:03 -0700

    make realistic persistence calls

--- src/k2/module/k23si/Persistence.cpp
+++ src/k2/module/k23si/Persistence.cpp
@@ -1,6 +1,4 @@
 #include "Persistence.h"
-#include <k2/dto/K23SI.h>
-#include <k2/dto/MessageVerbs.h>
 
 namespace k2 {
 
@@ -8,20 +6,6 @@ Persistence::Persistence() {
     //TODO discover RDMA endpoint
     _remoteEndpoint = RPC().getTXEndpoint(_config.persistenceEndpoint());
     K2INFO("ctor with endpoint: " << _remoteEndpoint->getURL());
-}
-
-seastar::future<> Persistence::makeCall(FastDeadline deadline) {
-    //TODO fix all users to use persistence appropriately. For now, we just mock a remote network call
-    dto::K23SI_PersistenceRequest request{};
-    if (_remoteEndpoint) {
-        K2DEBUG("making persistence call to endpoint: " << _remoteEndpoint->getURL() << ", with deadline=" << deadline.getRemaining());
-
-        return RPC().callRPC<dto::K23SI_PersistenceRequest, dto::K23SI_PersistenceResponse>
-            (dto::Verbs::K23SI_Persist, request, *_remoteEndpoint, deadline.getRemaining()).discard_result();
-    }
-    else {
-        return seastar::make_exception_future(std::runtime_error("Persistence not availabe"));
-    }
 }
 
 }
--- src/k2/module/k23si/Persistence.h
+++ src/k2/module/k23si/Persistence.h
@@ -1,12 +1,30 @@
 #pragma once
 #include <k2/appbase/AppEssentials.h>
+#include <k2/dto/K23SI.h>
+#include <k2/dto/MessageVerbs.h>
 #include "Config.h"
 
 namespace k2 {
 class Persistence {
 public:
     Persistence();
-    seastar::future<> makeCall(FastDeadline deadline);
+    template<typename ValueType>
+    seastar::future<> makeCall(const ValueType& val, FastDeadline deadline) {
+        return seastar::do_with(dto::K23SI_PersistenceRequest<Payload>{}, [&val, this, deadline] (auto& request) {
+            if (_remoteEndpoint) {
+                auto payload = _remoteEndpoint->newPayload();
+                payload->write(val);
+                request.value.val = std::move(*payload);
+                K2DEBUG("making persistence call to endpoint: " << _remoteEndpoint->getURL() << ", with deadline=" << deadline.getRemaining());
+
+                return RPC().callRPC<dto::K23SI_PersistenceRequest<Payload>, dto::K23SI_PersistenceResponse>
+                    (dto::Verbs::K23SI_Persist, request, *_remoteEndpoint, deadline.getRemaining()).discard_result();
+            }
+            else {
+                return seastar::make_exception_future(std::runtime_error("Persistence not availabe"));
+            }
+        });
+    }
 private:
     std::unique_ptr<TXEndpoint> _remoteEndpoint;
     K23SIConfig _config;
