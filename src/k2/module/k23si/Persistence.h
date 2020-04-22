d4e2474b886e97aee5db833b569a5769eb8f3f38
Ivan Avramov <iavramov@futurewei.com>
2020-04-22 09:40:36 -0700

    mr fix

--- src/k2/module/k23si/Persistence.h
+++ src/k2/module/k23si/Persistence.h
@@ -10,20 +10,20 @@ public:
     Persistence();
     template<typename ValueType>
     seastar::future<> makeCall(const ValueType& val, FastDeadline deadline) {
-        return seastar::do_with(dto::K23SI_PersistenceRequest<Payload>{}, [&val, this, deadline] (auto& request) {
-            if (_remoteEndpoint) {
-                auto payload = _remoteEndpoint->newPayload();
-                payload->write(val);
-                request.value.val = std::move(*payload);
-                K2DEBUG("making persistence call to endpoint: " << _remoteEndpoint->getURL() << ", with deadline=" << deadline.getRemaining());
-
+        if (_remoteEndpoint) {
+            auto payload = _remoteEndpoint->newPayload();
+            payload->write(val);
+            dto::K23SI_PersistenceRequest<Payload> request{};
+            request.value.val = std::move(*payload);
+            K2DEBUG("making persistence call to endpoint: " << _remoteEndpoint->getURL() << ", with deadline=" << deadline.getRemaining());
+            return seastar::do_with(std::move(request), [this, deadline] (auto& request) {
                 return RPC().callRPC<dto::K23SI_PersistenceRequest<Payload>, dto::K23SI_PersistenceResponse>
                     (dto::Verbs::K23SI_Persist, request, *_remoteEndpoint, deadline.getRemaining()).discard_result();
-            }
-            else {
-                return seastar::make_exception_future(std::runtime_error("Persistence not availabe"));
-            }
-        });
+            });
+        }
+        else {
+            return seastar::make_exception_future(std::runtime_error("Persistence not availabe"));
+        }
     }
 private:
     std::unique_ptr<TXEndpoint> _remoteEndpoint;
