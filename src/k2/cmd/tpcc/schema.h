ea9e4b0dde8ebd75f806a3dc958f1412df73c889
YazhiFeng <fengyazhi@huawei.com>
2020-10-16 15:29:56 +0800

    try TPCC partialUpdate for warehouse update in PaymentT

--- src/k2/cmd/tpcc/schema.h
+++ src/k2/cmd/tpcc/schema.h
@@ -56,6 +56,19 @@ seastar::future<k2::WriteResult> writeRow(ValueType& row, k2::K2TxnHandle& txn)
     });
 }
 
+template<typename ValueType>
+seastar::future<k2::PartialUpdateResult> 
+partialUpdateRow<Warehouse>(ValueType& row, std::vector<uint32_t> fieldsToUpdate, k2::K2TxnHandle& txn) {
+    return txn.partialUpdate<ValueType>(row, fieldsToUpdate).then([] (k2::PartialUpdateResult&& result) {
+        if (!result.status.is2xxOK()) {
+            K2DEBUG("partialUpdateRow failed: " << result.status);
+            return seastar::make_exception_future<k2::PartialUpdateResult>(std::runtime_error("partialUpdateRow failed!"));
+        }
+
+        return seastar::make_ready_future<k2::PartialUpdateResult>(std::move(result));
+    });
+}
+
 struct Address {
     Address () = default;
     Address (RandomContext& random) {
