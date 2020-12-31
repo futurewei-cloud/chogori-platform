d683fde1da50069e059e8a365be78c3a480a0ce3
Ivan Avramov <iavramov@futurewei.com>
2020-12-30 16:29:25 -0800

    b64 encode binary strings for json responses

--- src/k2/common/Common.h
+++ src/k2/common/Common.h
@@ -23,50 +23,48 @@ Copyright(c) 2020 Futurewei Cloud
 
 #pragma once
 
+#include <algorithm>
 #include <functional>
-#include <memory>
 #include <iomanip>
 #include <iostream>
-
-#include <seastar/core/temporary_buffer.hh>
+#include <memory>
+#include <nlohmann/json.hpp>
 #include <seastar/core/shared_ptr.hh>
 #include <seastar/core/sstring.hh>
-
-#include <nlohmann/json.hpp>
+#include <seastar/core/temporary_buffer.hh>
 
 #include "Chrono.h"
 
-#define DISABLE_COPY(className)                                 \
-    className(const className&) = delete;                       \
-    className& operator=(const className&) = delete;            \
+#define DISABLE_COPY(className)           \
+    className(const className&) = delete; \
+    className& operator=(const className&) = delete;
 
-#define DISABLE_MOVE(className)                                 \
-    className(className&&) = delete;                            \
-    className& operator=(className&&) = delete;                 \
+#define DISABLE_MOVE(className)      \
+    className(className&&) = delete; \
+    className& operator=(className&&) = delete;
 
-#define DISABLE_COPY_MOVE(className)                            \
-    DISABLE_COPY(className)                                     \
-    DISABLE_MOVE(className)                                     \
+#define DISABLE_COPY_MOVE(className) \
+    DISABLE_COPY(className)          \
+    DISABLE_MOVE(className)
 
-#define DEFAULT_COPY(className)                                 \
-    className(const className&) = default;                      \
-    className& operator=(const className&) = default;           \
+#define DEFAULT_COPY(className)            \
+    className(const className&) = default; \
+    className& operator=(const className&) = default;
 
-#define DEFAULT_MOVE(className)                                 \
-    className(className&&) = default;                           \
-    className& operator=(className&&) = default;                \
+#define DEFAULT_MOVE(className)       \
+    className(className&&) = default; \
+    className& operator=(className&&) = default;
 
-#define DEFAULT_COPY_MOVE(className)                            \
-    DEFAULT_COPY(className)                                     \
-    DEFAULT_MOVE(className)                                     \
+#define DEFAULT_COPY_MOVE(className) \
+    DEFAULT_COPY(className)          \
+    DEFAULT_MOVE(className)
 
-#define DEFAULT_COPY_MOVE_INIT(className)                       \
-    className() {}                                              \
-    DEFAULT_COPY(className)                                     \
-    DEFAULT_MOVE(className)                                     \
+#define DEFAULT_COPY_MOVE_INIT(className) \
+    className() {}                        \
+    DEFAULT_COPY(className)               \
+    DEFAULT_MOVE(className)
 
-namespace k2
-{
+namespace k2 {
 
 //
 //  K2 general string type
@@ -88,14 +86,59 @@ typedef std::function<Binary()> BinaryAllocatorFunctor;
 // or  std::array<MyEnum, to_integral(MyEnum::Red)>;
 template <typename T>
 auto to_integral(T e) { return static_cast<std::underlying_type_t<T>>(e); }
-}   //  namespace k2
+
+// from https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c/34571089#34571089
+static const char* B64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+static const int B64index[256] =
+    {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62, 63, 62, 62, 63,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0, 0, 0, 0, 63,
+        0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
+
+inline const std::string b64encode(const void* data, const size_t len) {
+    std::string result((len + 2) / 3 * 4, '=');
+    char* p = (char*)data;
+    char* str = &result[0];
+    size_t j = 0, pad = len % 3;
+    const size_t last = len - pad;
+
+    for (size_t i = 0; i < last; i += 3) {
+        int n = int(p[i]) << 16 | int(p[i + 1]) << 8 | p[i + 2];
+        str[j++] = B64chars[n >> 18];
+        str[j++] = B64chars[n >> 12 & 0x3F];
+        str[j++] = B64chars[n >> 6 & 0x3F];
+        str[j++] = B64chars[n & 0x3F];
+    }
+    if (pad) {
+        int n = --pad ? int(p[last]) << 8 | p[last + 1] : p[last];
+        str[j++] = B64chars[pad ? n >> 10 & 0x3F : n >> 2];
+        str[j++] = B64chars[pad ? n >> 4 & 0x03F : n << 4 & 0x3F];
+        str[j++] = pad ? B64chars[n << 2 & 0x3F] : '=';
+    }
+    return result;
+}
+
+}  //  namespace k2
 
 namespace nlohmann {
 
 template <>
 struct adl_serializer<k2::String> {
     static void to_json(json& j, const k2::String& str) {
-        j = std::string(str);
+        if (std::count_if(str.begin(), str.end(), [](auto c) {
+                return !std::isprint(c);
+            }) == 0) {
+            // all are printable
+            j = std::string(str);
+        } else {
+            j = k2::b64encode(str.data(), str.size());
+        }
     }
 
     static void from_json(const json& j, k2::String& str) {
@@ -103,4 +146,4 @@ struct adl_serializer<k2::String> {
     }
 };
 
-} // namespace nlohmann
+}  // namespace nlohmann
