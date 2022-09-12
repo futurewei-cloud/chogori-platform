3af837a17b7098b0aff9d61d4ccc75c2c3beac46
Dawei Li <dli3@futurewei.com>
2022-09-12 07:37:56 +0000

    changing name only: Decimal128 to Decimal100

--- src/skvhttpclient/src/skvhttp/dto/FieldTypes.cpp
+++ src/skvhttpclient/src/skvhttp/dto/FieldTypes.cpp
@@ -45,7 +45,7 @@ template <> FieldType TToFieldType<float>() { return FieldType::FLOAT; }
 template <> FieldType TToFieldType<double>() { return FieldType::DOUBLE; }
 template <> FieldType TToFieldType<bool>() { return FieldType::BOOL; }
 template <> FieldType TToFieldType<Decimal64>() { return FieldType::DECIMAL64; }
-template <> FieldType TToFieldType<Decimal128>() { return FieldType::DECIMAL128; }
+template <> FieldType TToFieldType<Decimal100>() { return FieldType::DECIMAL100; }
 template <> FieldType TToFieldType<FieldType>() { return FieldType::FIELD_TYPE; }
 
 // All conversion assume ascending ordering
