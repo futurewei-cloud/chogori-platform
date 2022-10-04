b11d0ebc960e065c7d8f8c8630cc479160c18933
Ahsan Khan <akhan@futurewei.com>
2022-10-04 12:27:19 -0700

    Change based on code review

--- test/integration/run_k2_cluster.sh
+++ test/integration/run_k2_cluster.sh
@@ -15,11 +15,10 @@ CPO=${CPO:=tcp+k2rpc://0.0.0.0:9000}
 TSO=${TSO:=tcp+k2rpc://0.0.0.0:13000}
 COMMON_ARGS=${COMMON_ARGS:="--reactor-backend=epoll --enable_tx_checksum true --thread-affinity false"}
 HTTP=${HTTP:=tcp+k2rpc://0.0.0.0:20000}
-CPO_ADDITIONAL_PORT=${CPO_ADDITIONAL_PORT:=9001}
 PROMETHEUS_PORT_START=${PROMETHEUS_PORT_START:=63000}
 
 # start CPO
-cpo_main ${COMMON_ARGS} -c1 --tcp_endpoints ${CPO} ${CPO_ADDITIONAL_PORT} --data_dir ${CPODIR} --prometheus_port $PROMETHEUS_PORT_START --assignment_timeout=1s --txn_heartbeat_deadline=1s --nodepool_endpoints ${EPS[@]} --tso_endpoints ${TSO} --tso_error_bound=100us --persistence_endpoints ${PERSISTENCE}&
+cpo_main ${COMMON_ARGS} -c1 --tcp_endpoints ${CPO}  --data_dir ${CPODIR} --prometheus_port $PROMETHEUS_PORT_START --assignment_timeout=1s --txn_heartbeat_deadline=1s --nodepool_endpoints ${EPS[@]} --tso_endpoints ${TSO} --tso_error_bound=100us --persistence_endpoints ${PERSISTENCE}&
 cpo_child_pid=$!
 
 # start nodepool
@@ -37,7 +36,7 @@ tso_child_pid=$!
 sleep 1
 
 # start http proxy
-http_proxy ${COMMON_ARGS} -c1 --tcp_endpoints ${HTTP} --memory=1G --cpo ${CPO} --httpproxy_txn_timeout=100h --httpproxy_expiry_timer_interval=50ms --log_level=Info &
+http_proxy ${COMMON_ARGS} -c1 --tcp_endpoints ${HTTP} --memory=1G --cpo ${CPO} --httpproxy_txn_timeout=100h --httpproxy_expiry_timer_interval=50ms --log_level=Info  --prometheus_port $(($PROMETHEUS_PORT_START+4)) &
 http_child_pid=$!
 
 function finish {
@@ -77,7 +76,7 @@ trap ign INT
 if [ -z "$1" ]
 then
     echo ">>>> Press CTRL+C to exit..."
-    wait ${http_child_pid}
+    sleep infinity
 else
     echo ">>>> Running" "${1}" "${@:2}"
     eval "${1}" "${@:2}"
