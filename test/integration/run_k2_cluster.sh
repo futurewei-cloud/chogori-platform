3246a71945e4dcf185aac7fadaac85e4fd4f69b2
Ivan Avramov <iavramov@futurewei.com>
2022-11-28 11:35:00 -0800

    use 3g mem for http proxy

--- test/integration/run_k2_cluster.sh
+++ test/integration/run_k2_cluster.sh
@@ -16,6 +16,7 @@ CPO=${CPO:=tcp+k2rpc://0.0.0.0:9000}
 TSO=${TSO:=tcp+k2rpc://0.0.0.0:13000}
 COMMON_ARGS=${COMMON_ARGS:="--reactor-backend=epoll --enable_tx_checksum true --thread-affinity false"}
 HTTP=${HTTP:=tcp+k2rpc://0.0.0.0:20000}
+HTTPMEM=${HTTPMEM:=3G}
 PROMETHEUS_PORT_START=${PROMETHEUS_PORT_START:=63000}
 
 # start CPO
@@ -35,7 +36,7 @@ tso ${COMMON_ARGS} -c1 --tcp_endpoints ${TSO} --prometheus_port $(($PROMETHEUS_P
 tso_child_pid=$!
 
 # start http proxy
-http_proxy ${COMMON_ARGS} -c1 --tcp_endpoints ${HTTP} --memory=1G --cpo ${CPO} --httpproxy_txn_timeout=100h --httpproxy_expiry_timer_interval=50ms --log_level=Info  --prometheus_port $(($PROMETHEUS_PORT_START+4)) &
+http_proxy ${COMMON_ARGS} -c1 --tcp_endpoints ${HTTP} --memory=${HTTPMEM} --cpo ${CPO} --httpproxy_txn_timeout=100h --httpproxy_expiry_timer_interval=50ms --log_level=Info  --prometheus_port $(($PROMETHEUS_PORT_START+4)) &
 http_child_pid=$!
 
 function finish {
@@ -79,4 +80,3 @@ else
     echo ">>>> Running" "${1}" "${@:2}"
     eval "${1}" "${@:2}"
 fi
-
