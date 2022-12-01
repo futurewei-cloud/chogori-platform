79dd57c182812e81ba844722a5dcff6383b67779
Dawei Li <dli3@futurewei.com>
2022-12-01 23:11:46 +0000

    update field types in skvclient to match with platform

--- test/integration/skvclient.py
+++ test/integration/skvclient.py
@@ -72,12 +72,15 @@ class FieldType(int, Enum):
     INT16T:     int     = 2
     INT32T:     int     = 3
     INT64T:     int     = 4
-    FLOAT:      int     = 5
-    DOUBLE:     int     = 6
-    BOOL:       int     = 7
-    DECIMALD25:  int     = 8
-    DECIMALD50:  int     = 9
-    DECIMALD100: int     = 10
+    UINT16T:    int     = 5
+    UINT32T:    int     = 6
+    UINT64T:    int     = 7
+    FLOAT:      int     = 8
+    DOUBLE:     int     = 9
+    BOOL:       int     = 10
+    DECIMALD25:  int     = 11
+    DECIMALD50:  int     = 12
+    DECIMALD100: int     = 13
 
     def serialize(self):
         return self.value
